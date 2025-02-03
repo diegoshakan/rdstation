a4c7dd5e4070ab8c70d6ae566ac4aa2f508248fa
Diego Araujo <diego_shakan@hotmail.com>
2025-02-03 13:41:21 -0300

    test: job

--- spec/factories/carts.rb
+++ spec/factories/carts.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :cart do
     total_price { FFaker.numerify('##.##') }
+    last_interaction_at { Time.current }
+
+    trait :abandoned do
+      abandoned_at { Time.current }
+    end
+
+    trait :cart_abandoned do
+      abandoned_at { 8.days.ago }
+    end
   end
 end
--- spec/jobs/abandoned_carts_job_spec.rb
+++ spec/jobs/abandoned_carts_job_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe AbandonedCartsJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#perform' do
+    context 'when there are carts without interaction for more than 3 hours' do
+      it 'marks carts as abandoned' do
+        cart = create(:cart, last_interaction_at: 4.hours.ago)
+        AbandonedCartsJob.perform_now
+
+        expect(cart.reload.abandoned_at).not_to be_nil
+      end
+    end
+
+    context 'when there are carts abandoned for more than 7 days' do
+      it 'destroys carts' do
+        cart = create(:cart, :abandoned, :cart_abandoned)
+        AbandonedCartsJob.perform_now
+
+        expect(Cart.exists?(cart.id)).to be_falsey
+      end
+    end
+
+    context 'when there are no carts to update or destroy' do
+      it 'does nothing' do
+        cart = create(:cart, last_interaction_at: 1.hour.ago)
+        AbandonedCartsJob.perform_now
+
+        expect(cart.reload.abandoned_at).to be_nil
+        expect(Cart.exists?(cart.id)).to be_truthy
+      end
+    end
+  end
 end
